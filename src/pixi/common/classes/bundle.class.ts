176a967a914ec9da4e1c8934236dd5ffab38ca28
Giorgi Maziashvili <giorgim@redberry.ge>
2025-03-12 23:16:39 +0400

    refactor: Remove unused code and clean up BundleClass implementation

--- src/pixi/common/classes/bundle.class.ts
+++ src/pixi/common/classes/bundle.class.ts
@@ -1,5 +1,4 @@
 import { Assets } from "pixi.js";
-<<<<<<< HEAD
 
 interface IBundles {
     name: string,
@@ -16,19 +15,5 @@ export class BundleClass{
         bundles.forEach(bundle=>{
             Assets.addBundle(bundle.name,bundle.assets)
         })
-=======
-import { IBundle } from "../constants/bundles.constant";
-
-export class BundleClass{
-    load(bundleIds:IBundle[]){
-        this.addBundle(bundleIds);
-        return Assets.loadBundle('slot');
-    }
-
-    addBundle(bundles:IBundle[]){
-        bundles.forEach((bundle:IBundle) => {
-            Assets.addBundle(bundle.alias,bundle.manifest);
-        });
->>>>>>> 46c54d192fe282cc1499a774a8911342a65847e0
     }
 }
